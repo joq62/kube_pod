4dece3f787940d26cd7579324606c87e407c514c
joq62 <joakim.leche@gmail.com>
2021-08-28 09:40:41 +0200

    created

---
+++ Makefile
@@ -0,0 +1,41 @@
+all:
+#	service
+	rm -rf ebin *_ebin lgh *.lgh;
+	mkdir ebin;
+	erlc -I ../interfaces -o ebin src/*.erl;
+	rm -rf ebin src/*.beam *.beam  test_src/*.beam test_ebin;
+	rm -rf  *~ */*~  erl_cra*;
+	rm -rf *_specs *_config *.log;
+	echo Done
+unit_test:
+	rm -rf rm lgh_ebin;
+	rm -rf src/*.beam *.beam test_src/*.beam test_ebin;
+	rm -rf  *~ */*~  erl_cra*;
+	mkdir lgh_ebin;
+	mkdir test_ebin;
+#	interface
+	erlc -I ../interfaces -o lgh_ebin ../interfaces/*.erl;
+#	support
+	cp ../applications/support/src/*.app lgh_ebin;
+	erlc -I ../interfaces -o lgh_ebin ../kube_support/src/*.erl;
+	erlc -I ../interfaces -o lgh_ebin ../applications/support/src/*.erl;
+#	etcd
+	cp ../applications/etcd/src/*.app lgh_ebin;
+	erlc -I ../interfaces -o lgh_ebin ../kube_dbase/src/*.erl;
+	erlc -I ../interfaces -o lgh_ebin ../applications/etcd/src/*.erl;
+#	kubelet
+	cp ../applications/kubelet/src/*.app lgh_ebin;
+	erlc -I ../interfaces -o lgh_ebin ../node/src/*.erl;
+	erlc -I ../interfaces -o lgh_ebin ../applications/kubelet/src/*.erl;
+#	pod
+	erlc -I ../interfaces -o lgh_ebin src/*.erl;
+#	test application
+	cp test_src/*.app test_ebin;
+	erlc -I ../interfaces -o test_ebin test_src/*.erl;
+	erl -pa lgh_ebin -pa test_ebin\
+	    -setcookie lgh_cookie\
+	    -sname pod_lgh\
+	    -unit_test monitor_node pod_lgh\
+	    -unit_test cluster_id lgh\
+	    -unit_test start_host_id c0_lgh\
+	    -run unit_test start_test test_src/test.config
---
+++ test_src/app_start_test.erl
@@ -0,0 +1,88 @@
+%%% -------------------------------------------------------------------
+%%% Author  : uabjle
+%%% Description : dbase using dets 
+%%% 
+%%% Created : 10 dec 2012
+%%% -------------------------------------------------------------------
+-module(app_start_test).    
+   
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+%% --------------------------------------------------------------------
+-define(APP,iaas).
+%% External exports
+-export([start/0]).
+
+
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function:tes cases
+%% Description: List of test cases 
+%% Returns: non
+%% --------------------------------------------------------------------
+start()->
+    ?debugMsg("Start setup"),
+    ?assertEqual(ok,setup()),
+    ?debugMsg("stop setup"),
+
+ %   ?debugMsg("Start testXXX"),
+ %   ?assertEqual(ok,single_node()),
+ %   ?debugMsg("stop single_node"),
+    
+      %% End application tests
+    ?debugMsg("Start cleanup"),
+    ?assertEqual(ok,cleanup()),
+    ?debugMsg("Stop cleanup"),
+
+    ?debugMsg("------>"++atom_to_list(?MODULE)++" ENDED SUCCESSFUL ---------"),
+    ok.
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
+
+setup()->
+
+    {ok,ClusterIdAtom}=application:get_env(unit_test,cluster_id),
+    ClusterId=atom_to_list(ClusterIdAtom),
+    os:cmd("rm -rf "++ClusterId),
+    ok=file:make_dir(ClusterId),
+    {ok,MonitorNodeNameAtom}=application:get_env(unit_test,monitor_node),
+    MonitorNodeName=atom_to_list(MonitorNodeNameAtom),
+    {ok,HostId}=inet:gethostname(),
+    MonitorNode=list_to_atom(MonitorNodeName++"@"++HostId),
+    Env=[{cluster_id,ClusterIdAtom},{monitor_node,MonitorNode}],
+    ok=application:set_env([{support,Env},
+			    {kubelet,Env},
+			    {etcd,Env}]),
+    ok=application:start(support),
+    ok=application:start(etcd),
+    ok=application:start(kubelet),
+    ok.
+
+
+
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% -------------------------------------------------------------------    
+
+cleanup()->
+  
+  %  init:stop(),
+    ok.
+%% --------------------------------------------------------------------
+%% Function:start/0 
+%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Returns: non
+%% --------------------------------------------------------------------
